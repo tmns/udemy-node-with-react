d01bcf7a1058c950464efff9b3a2b73aea39ef9c
sairim <spiri@protonmail.com>
2019-04-07 15:17:44 +0300

    add work from section 9 which involved configuring Express to route requests to React properly

--- course-project/server/index.js
+++ course-project/server/index.js
@@ -29,6 +29,18 @@ app.use(passport.session());
 require('./routes/authRoutes')(app);
 require('./routes/billingRoutes')(app);
 
+// make sure express handles routes in production correctly
+if (process.env.NODE_ENV == 'production') {
+    // Express will serve up production assets like our main.js & main.css files
+    app.use(express.static('client/build'));
+
+    // Express will serve up the index.html file if it doesn't recognize the route
+    const path = require('path');
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
+    });
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
 
